4ca0124c6c496b6bdcc0a91ac045a89182b8d47d
Lorena Igartua <lorenaigartua@gmail.com>
2017-09-29 20:53:47 -0300

    prettyURL (armado del ConfigApp y el route)

---
+++ config/ConfigApp.php
@@ -0,0 +1,15 @@
+<?php
+
+class ConfigApp
+{
+  public static $ACTION = "action";
+  public static $PARAMS = "params";
+  public static $ACTIONS = [
+      '/' =>  'nosotros',
+      'nosotros' =>  'nosotros',
+      'menu' =>  'menu',
+      'contacto' =>  'contacto',
+  ];
+}
+
+ ?>
--- route.php
+++ route.php
@@ -1,22 +1,17 @@
 <?php
-require_once ('libraries/About.php');
-require_once ('libraries/Operations.php');
-require_once ('libraries/Constants.php');
-require_once ('config/ConfigApp.php');
+define('ACTION', 0);
+define('VALOR1', 1);
+include_once 'config/ConfigApp.php';
+include_once 'index.php';
 
 function parseUrl($url){
-  $arr_data = explode ("/",$url);
-  // Se guarda el nombre de la accion
-  $arrayReturn[ConfigApp::$ACTION] = $arr_data[0];
-  // Se guarda la lista de parametros como un array
-  $arrayReturn[ConfigApp::$PARAMS] = isset($arr_data[1]) ? array_slice($arr_data, 1) : null;
-
+  $urlExploded = explode('/', $url);
+  $arrayReturn[ConfigApp::$ACTION] = $urlExploded[ACTION];
+  $arrayReturn[ConfigApp::$PARAMS] = isset($urlExploded[VALOR1]) ? array_slice($urlExploded,1) : null;
   return $arrayReturn;
 }
 
-// Parsear la URL para identificar Actions y Parametros
-$urlData = parseUrl($_GET[ConfigApp::$ACTION]);
-// Nombre de la Accion a ejecutar
+$urlData = parseUrl('action');
 $actionName = $urlData[ConfigApp::$ACTION];
 
 if (array_key_exists($actionName,ConfigApp::$ACTIONS)){
@@ -24,13 +19,11 @@ if (array_key_exists($actionName,ConfigApp::$ACTIONS)){
     $methodName = ConfigApp::$ACTIONS[$actionName];
 
     if(isset($params) && $params != null){
-      // Invocar el metodo con el array de parametros
       $methodName($params);
     } else {
       $methodName();
     }
 } else {
-    // No existe la Action entonces muestro la Home
-    mostrar_about();
+    home();
 }
  ?>
