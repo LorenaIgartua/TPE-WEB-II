0465eca7d654f4683e9ba05356d80c31c3f66d5c
Lorena Igartua <lorenaigartua@gmail.com>
2017-09-29 21:08:41 -0300

    modifique funciones del index.php (en prueba)

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <?php
 
+include_once 'libs/Smarty.class.php';
+
 include_once 'route.php';
 
 function home () {
   echo 'hola';
 }
+
+function hola () {
+  $smarty = new Smarty();
+  $smarty->assign('titulo',"RISOTERIA ITALIANA");
+  $smarty->display('templates/home.tpl');
+}
+
+function contacto () {
+  $smarty = new Smarty();
+  $smarty->assign('titulo',"RISOTERIA ITALIANA");
+  $smarty->display('templates/contacto.tpl');
+}
+
+function nosotros () {
+  echo 'nosotros';
+}
+
 
 
 ?>
